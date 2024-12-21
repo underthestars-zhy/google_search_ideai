179d17225ef8354222be18367d49ed4f60a4413f
underthestars-zhy <zhuhaoyu0909@icloud.com>
2024-12-20 21:59:17 -0500

    Set dynamic proxy assignment for outgoing HTTP(S) requests.
    Moved proxy environment variable setup into the request-handling logic, generating random ports dynamically for GEONODE_DNS. This change enhances connection variability and could improve load balancing or reduce predictable behavior.

--- main.py
+++ main.py
@@ -6,11 +6,10 @@ from starlette.middleware.cors import CORSMiddleware
 
 username = "geonode_WRVJYKoq40"
 password = "59eb5a54-bbf9-4096-9385-5323b360079f"
-GEONODE_DNS = "us.premium-residential.geonode.com:9000"
+
 
 # Set proxies as environment variables
-os.environ["http_proxy"] = f"http://{username}:{password}@{GEONODE_DNS}"
-os.environ["https_proxy"] = f"https://{username}:{password}@{GEONODE_DNS}"
+
 
 app = FastAPI()
 
@@ -28,5 +27,12 @@ class Item(BaseModel):
 
 @app.post("/")
 def search_google(item: Item) -> list[str]:
+    import random
+
+    GEONODE_DNS = f"us.premium-residential.geonode.com:{random.randint(9002, 9008)}"
+
+    os.environ["http_proxy"] = f"http://{username}:{password}@{GEONODE_DNS}"
+    os.environ["https_proxy"] = f"https://{username}:{password}@{GEONODE_DNS}"
+    
     results = search(item.query, num=item.num, stop=item.num)
     return results
